116c3e40b19a7d2e7770b78f70d60d011a99c815
Tuan Tran <petuexuaf@gmail.com>
2022-04-22 00:37:33 +0700

    ''

--- js/main.js
+++ js/main.js
@@ -3,10 +3,42 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const trunc = (string,limitNum) => {
-    return string.length > limitNum ? string.slice(0,limitNum) + '...' : string
-}
+const trunc = (string, limitNum) => {
+  return string.length > limitNum ? string.slice(0, limitNum) + "..." : string;
+};
 
+const nextBtn = $(".slider__direction__btn.next");
+const prevBtn = $(".slider__direction__btn.prev");
+const slides = $(".slides");
+const slideWidth = $$(".slide__img__wrapper")[0].offsetWidth + 20;
+const carouselHandler = (nextBtn,prevBtn,slides,slideWidth) => {
+  let movePer = 0;
+  let index = 0;
+  const clickSlideHanlder = (direction) => {
+    if (direction === 1) {
+      if (index === 3) {
+        return;
+      }
+      movePer -= slideWidth;
+      slides.style = `transform: translateX(${movePer}px);`;
+      index++;
+    }
+    if (direction === -1) {
+      if (index === 0) {
+        return;
+      }
+      movePer += slideWidth;
+      slides.style = `transform: translateX(${movePer}px);`;
+      index--;
+    }
+  };
+  nextBtn.onclick = () => {
+    clickSlideHanlder(1);
+  };
+  prevBtn.onclick = () => {
+    clickSlideHanlder(-1);
+  };
+};
 
 const cateFeature = {
   data: [
@@ -134,55 +166,55 @@ const dailyBg = {
     {
       label: "Trả góp 0%",
       img: 1,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Chú Thuật Hồi Chiến - Tập 2",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 2,
-      name: "Realme C35 64GB",
-      price: "3.990.000",
+      name: "Dạy Con Làm Giàu - Tập 1 (Tái Bản 2018)",
+      price: "3.990",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 3,
-      name: "Realme 9 Pro 5G",
-      price: "7.490.000",
+      name: "Dạy Con Làm Giàu 02 - Sử Dụng Đồng Vốn (Tái Bản 2020)",
+      price: "7.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 4,
-      name: "iPhone 11 64GB",
-      price: "16.290.000",
+      name: "Người Bán Hàng Vĩ Đại Nhất Thế Giới",
+      price: "16.290",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 5,
-      name: "iPhone 13 Pro Max 128GB",
-      price: "33.990.000",
+      name: "Netflix: Phá Bỏ Nguyên Tắc Để Bứt Phá - No Rules Rules",
+      price: "33.990",
       rate: 4.6,
       amountRate: 341,
     },
     {
       label: "Trả góp 0%",
       img: 6,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Chiến Tranh Tiền Tệ - Phần 1",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 7,
-      name: "OPPO Reno7 5G",
+      name: "Báo Cáo Tài Chính Dưới Góc Nhìn Của Warren Buffett (Tái Bản)",
       price: "12.990.000₫",
       rate: 3.8,
       amountRate: 73,
@@ -190,104 +222,104 @@ const dailyBg = {
     {
       label: "Trả góp 0%",
       img: 8,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Bước Đi Ngẫu Nhiên Trên Phố Wall (Tái Bản 2021)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 9,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Nhà Đầu Tư Thông Minh (Tái Bản 2020)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 10,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Báo Cáo Tài Chính ",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 11,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Tiếp Thị 4.0 - Dịch Chuyển Từ Truyền Thống Sang Công Nghệ",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 12,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Phân Tích Chứng Khoán (Security Analysis)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 13,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Từ Tốt Đến Vĩ Đại - Jim Collins (Tái Bản 2021)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 14,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Một Đời Quản Trị (Tái Bản 2019)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 15,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Kinh Doanh - Khái Lược Những Tư Tưởng Lớn",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 16,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Trên Đỉnh Phố Wall (Tái Bản 2021)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 17,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Đầu Tư Tài Chính (Tái Bản)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 18,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Bán Hàng Shopee Thực Chiến Từ A-Z",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 19,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Digital Marketing - Từ Chiến Lược Đến Thực Thi (Tái Bản)",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
     {
       label: "Trả góp 0%",
       img: 20,
-      name: "OPPO Reno7 Z 5G",
-      price: "10.490.000",
+      name: "Bán Hàng Cho Người Giàu",
+      price: "10.490",
       rate: 3.8,
       amountRate: 73,
     },
@@ -322,42 +354,60 @@ const dailyBg = {
 
 const techNew = {
   blogData: [
-      {
-          img: 1,
-          content: " AirPods 2 chưa bao giờ là hết HOT, lại còn đang được giảm tiền triệu tại Thế Giới Di Động, không mua uổng lắm"
-      },
-      {
-          img: 2,
-          content: 'Mùa mưa sắp tới rồi, tham khảo ngay 5 điện thoại chống nước đáng mua nhất với đủ phân khúc giá từ tầm trung tới cao cấp'
-      },
-      {
-          img: 3,
-          content: 'Đón lễ lớn giảm tối đa đến 3.7 triệu đồng, iPhone 12 128GB giá hời quá xá, mua ngay bây giờ bạn ơi!'
-      },
+    {
+      img: 1,
+      content:
+        " AirPods 2 chưa bao giờ là hết HOT, lại còn đang được giảm tiền triệu tại Thế Giới Di Động, không mua uổng lắm",
+    },
+    {
+      img: 2,
+      content:
+        "Mùa mưa sắp tới rồi, tham khảo ngay 5 điện thoại chống nước đáng mua nhất với đủ phân khúc giá từ tầm trung tới cao cấp",
+    },
+    {
+      img: 3,
+      content:
+        "Đón lễ lớn giảm tối đa đến 3.7 triệu đồng, iPhone 12 128GB giá hời quá xá, mua ngay bây giờ bạn ơi!",
+    },
   ],
   render() {
-      const techNewBlogList = $('.techNew__blog__list')
-      let htmls = this.blogData.map((blog,index)=>{
-          return `
+    const techNewBlogList = $(".techNew__blog__list");
+    let htmls = this.blogData.map((blog, index) => {
+      return `
           <div class="c-4 gutter">
                 <a href="#" class="techNew__blog__wrapper">
                     <span class="techNew__blog__img__wrapepr">
-                        <img src="./assets/img/techNew/blog/${blog.img}.jpg" alt="1"
+                        <img src="./assets/img/techNew/blog/${
+                          blog.img
+                        }.jpg" alt="1"
                             class="techNew__blog__img">
                     </span>
                     <p class="techNew__blog__content">
-                        ${trunc(blog.content,68)}
+                        ${trunc(blog.content, 68)}
                     </p>
                 </a>
             </div>
-          `
-      })
-      techNewBlogList.innerHTML = htmls.join('')
+          `;
+    });
+    techNewBlogList.innerHTML = htmls.join("");
   },
 };
 
+const accountHandler = () => {
+    const openBtn = $('.header__top__account__btn')
+    const closeBtn = $('.popUp__login__btn.back')
+    const signUpBg = $('.popUp__login__wrapper__modal__background')
+    openBtn.onclick = () => {
+        signUpBg.style = `display: block;`
+    }
+    closeBtn.onclick = () => {
+        signUpBg.style = `display: none;`
+    }
+};
 const main = (() => {
   cateFeature.render();
   dailyBg.render();
   techNew.render();
+  carouselHandler(nextBtn,prevBtn,slides,slideWidth);
+  accountHandler()
 })();
